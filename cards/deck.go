982079b614d8aa10023061269690b3c62cb87b29
Mykola Piskovyi <mykola.piskovyi@apptentive.com>
2019-10-18 14:19:35 +0300

    read from file

--- cards/deck.go
+++ cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -39,6 +40,10 @@ func (d deck) deal(size int) (deck, deck) {
 	return dealCards(d, size)
 }
 
+func (d deck) shuffle() deck  {
+	return d
+}
+
 func (d deck) convertToBytes() []byte {
 	return []byte(d.toString())
 }
@@ -50,6 +55,16 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, d.convertToBytes(), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ", ")
+	return deck(s);
 }
 
 // --- helpers
--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,11 @@
 package main
 
 func main() {
+	filename := "file_cards";
 	cards := newDeck()
 	_, remainigCards := cards.deal(3)
 
-	// hand.print()
-	// remainigCards.print()
+	remainigCards.saveToFile(filename)
 
-	remainigCards.saveToFile("file1cards")
-
-	// fmt.Println(remainigCards.convertToBytes())
+	newDeckFromFile(filename).print()
 }
